9d0c76ce3556a4689da480255c19d10c5f7e7983
gaoran_10@126.com <gaoran_10@126.com>
2025-09-26 14:46:55 +0800

    fix

--- external-schemas/java/src/main/java/io/streamnative/examples/exschema/json/Configurations.java
+++ external-schemas/java/src/main/java/io/streamnative/examples/exschema/json/Configurations.java
@@ -20,9 +20,9 @@ import java.util.Map;
 
 public class Configurations {
 
-    protected static final String TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImM4MjE4ZDUyLWViMjktNTY0Mi04YTc1LTRkNzkyMjY3MzVkYiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsidXJuOnNuOnB1bHNhcjpvLTJoMDU2OnVuaWZpZWQtc2NoZW1hIl0sImV4cCI6MTc2MTQ0NzUxOCwiaHR0cHM6Ly9zdHJlYW1uYXRpdmUuaW8vc2NvcGUiOlsiYWRtaW4iLCJhY2Nlc3MiXSwiaHR0cHM6Ly9zdHJlYW1uYXRpdmUuaW8vdXNlcm5hbWUiOiJyZ2FvQG8tMmgwNTYuYXV0aC5zbmNsb3VkLXN0Zy5kZXYiLCJpYXQiOjE3NTg4NTU1MjIsImlzcyI6Imh0dHBzOi8vcGMtYzQxZDM3OGEuYXdzLXVzZTEtdGVzdC1pMjdwNS5hd3Muc24zLmRldi9hcGlrZXlzLyIsImp0aSI6ImEzOTY1ZTcwZTY1MzRiOTU5Nzg2ZTVlMzkyOTI3MThlIiwicGVybWlzc2lvbnMiOltdLCJzdWIiOiJzNFVUd21URDhDd2FyU0RPWDAzRVJMVXhNUHNOUFpvYkBjbGllbnRzIn0.Ip6G40R4DJdwJKAQt3Q4N163R_onDn_oWsGC1I58oLjwk6NzL5-GCMhhCXiA8FJGkHjnRSFidOwXeQ_iYOUSzGLfjCIJCdaHTNmBLTmjVm-tgAz4OZ4ru40-nEe4Gml638AAF3c_7ujwkJ4B4aVbuQhlSEORDUn_xOXh22xyH3VqX2phj20bI7LEx1CFauHIHdJFrydvMgH7tuiyqwIcp7MfuEGsnGFSO9JPQLA7kbwHcKGm2CjPHr1b8wMVDgK2wHA07vCsXZNKWoEcO6qR5bS93wiJhk1n3A2znCnMvzWethBo6D9v8x6j8GCgJTLb8WWaYOUOKWdckMi6enSC_Q";
-    protected static final String PULSAR_SERVICE_URL = "pulsar+ssl://pc-c41d378a.aws-use1-test-i27p5.aws.sn3.dev:6651";
-    private static final String SCHEMA_REGISTRY_URL = "https://pc-c41d378a.aws-use1-test-i27p5.aws.sn3.dev/kafka";
+    protected static final String TOKEN = "<YOUR-TOKEN>";
+    protected static final String PULSAR_SERVICE_URL = "<PULSAR-SERVICE-URL>";
+    private static final String SCHEMA_REGISTRY_URL = "<SCHEMA-REGISTRY-URL>";
 
     public static Map<String, Object> getSchemaRegistryConfigs() {
         var map = new HashMap<String, Object>();
